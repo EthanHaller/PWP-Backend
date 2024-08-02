02663f13bfce552b47d8578a3083629c7135cbfa
EthanHaller <ttk4ey@virginia.edu>
2024-08-01 21:19:45 -0400

    Initial commit for backend

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+const { initializeApp } = require("firebase/app")
+const { getFirestore } = require("firebase/firestore")
+const { getAuth } = require("firebase/auth")
+
+const firebaseConfig = {
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	projectId: process.env.projectId,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.storageBucket,
+	appId: process.env.appId,
+	measurementId: process.env.measurementId,
+}
+
+const firebase = initializeApp(firebaseConfig)
+const db = getFirestore(firebase)
+const auth = getAuth(firebase);
+
+module.exports = { db, auth }
