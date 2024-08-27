3f4d54f28e6ea3ce58ec2a1147841e8a8587ae0c
EthanHaller <ttk4ey@virginia.edu>
2024-08-27 10:44:25 -0400

    feat: add updateRecruitmentInfo functionality

--- src/routes/recruitment.js
+++ src/routes/recruitment.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var router = express.Router()
-const { doc, getDoc } = require("firebase/firestore")
+const { doc, getDoc, updateDoc } = require("firebase/firestore")
 const { db } = require("../../netlify/functions/firebase-config")
 
 const getRecruitmentInfo = async (req, res) => {
@@ -18,5 +18,20 @@ const getRecruitmentInfo = async (req, res) => {
 	}
 }
 router.get("/", getRecruitmentInfo)
+
+const updateRecruitmentInfo = async (req, res) => {
+	try {
+		const recruitmentData = req.body
+		const recruitmentDocRef = doc(db, "recruitment", "generalInfo")
+
+		await updateDoc(recruitmentDocRef, recruitmentData)
+		const updatedRecruitmentDoc = await getDoc(recruitmentDocRef)
+
+		res.status(201).send({ ...updatedRecruitmentDoc.data() })
+	} catch (error) {
+		res.status(500).send(error.message)
+	}
+}
+router.put("/update", updateRecruitmentInfo)
 
 module.exports = router
