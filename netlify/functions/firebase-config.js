d1e0fc9f00a0f3489defae454a561db733722638
EthanHaller <ttk4ey@virginia.edu>
2024-08-27 10:53:31 -0400

    fix: update firebase service account to use env vars

--- netlify/functions/firebase-config.js
+++ netlify/functions/firebase-config.js
@@ -2,8 +2,7 @@ const { initializeApp } = require("firebase/app")
 const { getFirestore } = require("firebase/firestore")
 const { getStorage } = require("firebase/storage")
 const { getAuth } = require("firebase/auth")
-const admin = require('firebase-admin');
-const serviceAccount = require('../../serviceAccountKey.json');
+const admin = require("firebase-admin")
 
 const firebaseConfig = {
 	apiKey: process.env.apiKey,
@@ -15,9 +14,23 @@ const firebaseConfig = {
 	measurementId: process.env.measurementId,
 }
 
+const serviceAccount = {
+	type: "service_account",
+	project_id: process.env.FIREBASE_PROJECT_ID,
+	private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+	private_key: process.env.FIREBASE_PRIVATE_KEY,
+	client_email: process.env.FIREBASE_CLIENT_EMAIL,
+	client_id: process.env.FIREBASE_CLIENT_ID,
+	auth_uri: process.env.FIREBASE_AUTH_URI,
+	token_uri: process.env.FIREBASE_TOKEN_URI,
+	auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+	client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+	universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+}
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+	credential: admin.credential.cert(serviceAccount),
+})
 
 const firebase = initializeApp(firebaseConfig)
 const db = getFirestore(firebase)
